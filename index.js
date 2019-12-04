89c63211fec35a7af876a1cb59ba41ea110c9fd0
Matthew Stanciu <mattbstanciu@gmail.com>
2019-12-04 00:39:16 +0000

    Import code from Glitch

---
+++ index.js
@@ -0,0 +1,30 @@
+const Botkit = require('botkit')
+
+const controller = new Botkit.slackbot({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  clientSigningSecret: process.env.SIGNING_SECRET,
+  scopes: ['bot', 'chat:write:bot'],
+  debug: false
+})
+
+controller.spawn({
+  token: process.env.ACCESS_TOKEN
+})
+
+controller.setupWebserver(process.env.PORT || 3000, (err, webserver) => {
+  controller.createWebhookEndpoints(controller.webserver)
+  controller.createOauthEndpoints(controller.webserver)
+})
+
+controller.on('slash_command', (bot, message) => {
+  if (message.text.includes('@')) {
+    const id = message.text.split('|')[0].slice(2)
+    const user = message.text.split('|')[1]
+    const userTrimmed = user.slice(0, user.length-1)
+    
+    bot.replyPrivate(message, `<@${id}>'s User ID is ${id}`)
+  }
+  else
+    bot.replyPrivate(message, `${message.text}'s display name is <@${message.text}>`)
+})
